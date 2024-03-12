896a0d74e1e9a33187fd2329ee151f90390dd1ab
Matías Duhalde <contact@duhal.de>
2024-03-12 12:56:21 +0100

    chore: reorganize all plugins

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,15 +5,21 @@ return { -- Useful plugin to show you pending keybinds.
     require('which-key').setup()
 
     -- Document existing key chains
-    require('which-key').register {
+    local wk = require 'which-key'
+
+    wk.register {
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      ['<leader>gt'] = { name = '[T]oggle', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
     }
+    wk.register({
+      ['t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+    }, {
+      prefix = '<leader>g',
+    })
   end,
 }
 
